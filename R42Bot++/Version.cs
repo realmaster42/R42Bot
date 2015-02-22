0fac5b6afba7f9f85065885fb6ef86bc17a49acd
realmaster42 <totalrobloxian@hotmail.com>
2015-02-22 00:33:00 -0100

    Version.cs

---
+++ R42Bot++/Version.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerIOClient;
+
+namespace R42Bot
+{
+    public partial class Version
+    {
+        public static string versionlink = "https://dl.dropbox.com/s/6x0q6qmhd4afu40/version.txt";
+        public static string version = "1 - A new reborn!";
+        public static string upgradedVersion = "1 - A new reborn!";
+        public static string UpToDate = "";
+        public static string OutOfDate = "";
+    }
+}
