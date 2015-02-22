6c715809b7beccceee17f7874bc2d3109d8befb6
realmaster42 <totalrobloxian@hotmail.com>
2015-02-22 00:30:27 -0100

    Update and rename R42Bot++ to R42Bot++/Voids.cs

---
+++ R42Bot++/Voids.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerIOClient;
+
+namespace R42Bot
+{
+    public partial class Voids
+    {
+        #region Position Database Information <click to see>
+        public static string derot(string arg1)
+        {
+            int num = 0;
+            string str = "";
+            for (int i = 0; i < arg1.Length; i++)
+            {
+                num = arg1[i];
+                if ((num >= 0x61) && (num <= 0x7a))
+                {
+                    if (num > 0x6d)
+                    {
+                        num -= 13;
+                    }
+                    else
+                    {
+                        num += 13;
+                    }
+                }
+                else if ((num >= 0x41) && (num <= 90))
+                {
+                    if (num > 0x4d)
+                    {
+                        num -= 13;
+                    }
+                    else
+                    {
+                        num += 13;
+                    }
+                }
+                str = str + ((char)num);
+            }
+            return str;
+        }
+        #endregion
+
+        public static string Shortest(string name)
+        {
+            if (name.Length >= 5)
+            {
+                return name.Substring(0, 5);
+            }
+            else if (name.Length >= 3)
+            {
+                return name.Substring(0, 3);
+            }
+            return name;
+        }
+    }
+}
